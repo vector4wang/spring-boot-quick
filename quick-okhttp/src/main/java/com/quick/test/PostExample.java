a56daf400540685456a6e21297c5312f8f204d3a
vector <vector.wang@hirede.com>
2017-09-13 09:38:18 +0800

    调整了下okhttp相关代码，以后可能会用得着

--- quick-okhttp/src/main/java/com/quick/test/PostExample.java
+++ quick-okhttp/src/main/java/com/quick/test/PostExample.java
@@ -24,19 +24,7 @@ public class PostExample {
                 .post(body)
                 .build();
         Response response = client.newCall(request).execute();
-        response.body().string();
-    }
-
-    String bowlingJson(String player1, String player2) {
-        return "{'winCondition':'HIGH_SCORE',"
-                + "'name':'Bowling',"
-                + "'round':4,"
-                + "'lastSaved':1367702411696,"
-                + "'dateStarted':1367702378785,"
-                + "'players':["
-                + "{'name':'" + player1 + "','history':[10,8,6,7,8],'color':-13388315,'total':39},"
-                + "{'name':'" + player2 + "','history':[6,10,5,10,10],'color':-48060,'total':41}"
-                + "]}";
+        return response.body().string();
     }
 
     public static void main(String[] args) throws IOException {
