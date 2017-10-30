056ff9d45fc21ead6c867a1505eb170e28a0298b
vector <vector.wang@hirede.com>
2017-10-30 15:18:05 +0800

    改写oauth2使用redis保存token

--- quick-oauth2/quick-oauth2-server/src/main/java/com/quick/auth/server/security/AuthorizationServerConfiguration.java
+++ quick-oauth2/quick-oauth2-server/src/main/java/com/quick/auth/server/security/AuthorizationServerConfiguration.java
@@ -4,22 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 /**
  * 认证服务器配置
@@ -32,15 +25,17 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 	private String resourceId;
 
 	@Value("${access_token.validity_period:3600}") // 默认值3600
-			int accessTokenValiditySeconds = 3600;
+	int accessTokenValiditySeconds = 3600;
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
 
+	@Autowired
+	private RedisConnectionFactory redisConnectionFactory;
+
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.authenticationManager(this.authenticationManager);
-		endpoints.accessTokenConverter(accessTokenConverter());
 		endpoints.tokenStore(tokenStore());
 	}
 
@@ -69,44 +64,14 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 				.secret("secret");
 	}
 
-	/**
-	 * token converter
-	 *
-	 * @return
-	 */
-	@Bean
-	public JwtAccessTokenConverter accessTokenConverter() {
-		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter() {
-			/***
-			 * 重写增强token方法,用于自定义一些token返回的信息
-			 */
-			@Override
-			public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
-				String userName = authentication.getUserAuthentication().getName();
-				User user = (User) authentication.getUserAuthentication().getPrincipal();// 与登录时候放进去的UserDetail实现类一直查看link{SecurityConfiguration}
-				/** 自定义一些token属性 ***/
-				final Map<String, Object> additionalInformation = new HashMap<>();
-				additionalInformation.put("userName", userName);
-				additionalInformation.put("roles", user.getAuthorities());
-				((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInformation);
-				OAuth2AccessToken enhancedToken = super.enhance(accessToken, authentication);
-				return enhancedToken;
-			}
-
-		};
-		accessTokenConverter.setSigningKey("123");// 测试用,资源服务使用相同的字符达到一个对称加密的效果,生产时候使用RSA非对称加密方式
-		return accessTokenConverter;
-	}
 
 	/**
 	 * token store
 	 *
-	 * @param accessTokenConverter
 	 * @return
 	 */
 	@Bean
 	public TokenStore tokenStore() {
-		TokenStore tokenStore = new JwtTokenStore(accessTokenConverter());
-		return tokenStore;
+		return new RedisTokenStore(redisConnectionFactory);
 	}
 }
