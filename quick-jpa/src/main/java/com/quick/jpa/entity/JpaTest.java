d9d6c619ef1f9027c2d58c187b4487faa4625343
BMHJQS <772704457@qq.com>
2017-11-06 22:37:33 +0800

    添加了jpa模块，比较了下最快搭建操作数据库服务的速度 无论是写法还是速度，jpa都比较占上风

---
+++ quick-jpa/src/main/java/com/quick/jpa/entity/JpaTest.java
@@ -0,0 +1,52 @@
+package com.quick.jpa.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * @Author: wangxc
+ * @GitHub: https://github.com/vector4wang
+ * @CSDN: http://blog.csdn.net/qqhjqs?viewmode=contents
+ * @BLOG: http://vector4wang.tk
+ * @wxid: BMHJQS
+ */
+@Entity
+@Table(name = "jpa_test")
+public class JpaTest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "add_time")
+    private Date addTime;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getAddTime() {
+        return addTime;
+    }
+
+    public void setAddTime(Date addTime) {
+        this.addTime = addTime;
+    }
+}
---
+++ quick-jpa/src/main/java/com/quick/jpa/repository/JpaTestRepository.java
@@ -0,0 +1,14 @@
+package com.quick.jpa.repository;
+
+import com.quick.jpa.entity.JpaTest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @Author: wangxc
+ * @GitHub: https://github.com/vector4wang
+ * @CSDN: http://blog.csdn.net/qqhjqs?viewmode=contents
+ * @BLOG: http://vector4wang.tk
+ * @wxid: BMHJQS
+ */
+public interface JpaTestRepository extends JpaRepository<JpaTest,Integer> {
+}
