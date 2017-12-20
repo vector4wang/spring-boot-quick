117602dd39310cca9bdc57c122724de9c34834c4
vector <vector.wang@hirede.com>
2017-12-20 21:28:34 +0800

    一些补充

--- qucik-activemq/src/main/java/com/mq/Application.java
+++ qucik-activemq/src/main/java/com/mq/Application.java
@@ -2,17 +2,42 @@ package com.mq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+import javax.jms.ConnectionFactory;
 
 /**
  * Created with IDEA
  * User: vector
  * Data: 2017/12/20
  * Time: 17:31
- * Description:
+ * Description:http://javasampleapproach.com/spring-framework/spring-jms/activemq-create-springboot-activemq-response-management-application-sendto-annotation
  */
 @SpringBootApplication
 public class Application {
 
+    @Bean // Serialize message content to json using TextMessage
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+    @Bean
+    public JmsListenerContainerFactory<?> jsaFactory(ConnectionFactory connectionFactory,
+                                                     DefaultJmsListenerContainerFactoryConfigurer configurer) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setMessageConverter(jacksonJmsMessageConverter());
+        configurer.configure(factory, connectionFactory);
+        return factory;
+    }
 
 
 
