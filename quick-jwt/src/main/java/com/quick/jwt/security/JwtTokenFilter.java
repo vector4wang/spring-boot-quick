c36205e1df4e5af3b3cf8b517dbe339f8dbb5287
vector <vector.wang@hirede.com>
2018-12-26 10:45:03 +0800

    :smile:

--- quick-jwt/src/main/java/com/quick/jwt/security/JwtTokenFilter.java
+++ quick-jwt/src/main/java/com/quick/jwt/security/JwtTokenFilter.java
@@ -29,7 +29,7 @@ public class JwtTokenFilter extends GenericFilterBean {
 		String token = jwtTokenProvider.resolveToken((HttpServletRequest) req);
 		try {
 			if (token != null && jwtTokenProvider.validateToken(token)) {
-				Authentication auth = token != null ? jwtTokenProvider.getAuthentication(token) : null;
+				Authentication auth = jwtTokenProvider.getAuthentication(token);
 				SecurityContextHolder.getContext().setAuthentication(auth);
 			}
 		} catch (CustomException ex) {
