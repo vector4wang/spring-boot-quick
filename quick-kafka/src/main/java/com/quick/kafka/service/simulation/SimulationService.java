7eedceb6a31ae5b6ddeb08294621ee94fe9308a6
vector <vector4wang@qq.com>
2019-07-18 23:33:36 +0800

    更新 加了一些模拟数据

---
+++ quick-kafka/src/main/java/com/quick/kafka/service/simulation/SimulationService.java
@@ -0,0 +1,29 @@
+package com.quick.kafka.service.simulation;
+
+import com.quick.kafka.service.ProducerService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+/**
+ *
+ * @author wangxc
+ * @date: 2019/7/18 下午9:01
+ *
+ */
+@Service
+public class SimulationService {
+
+	@Resource
+	private ProducerService producerService;
+
+	/**
+	 * 只是测试，暂且不对body做验证，默认body一定不为空
+	 * @param topic
+	 * @param body
+	 */
+	public void send(String topic, String body) {
+		producerService.sendMessage(topic,body);
+	}
+}
