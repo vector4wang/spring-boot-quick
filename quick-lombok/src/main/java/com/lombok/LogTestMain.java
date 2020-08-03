2cbf3ffa2334f13f0e8c6cf973ec931a73f60424
vector4wang <772704457@qq.com>
2020-08-03 22:15:42 +0800

    1

--- quick-lombok/src/main/java/com/lombok/LogTestMain.java
+++ quick-lombok/src/main/java/com/lombok/LogTestMain.java
@@ -1,18 +1,21 @@
 package com.lombok;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+import java.util.Date;
 
 /**
- * @Log：根据不同的注解生成不同类型的log对象，但是实例名称都是log，有六种可选实现类
- *      @CommonsLog Creates log = org.apache.commons.logging.LogFactory.getLog(LogExample.class);
- *      @Log Creates log = java.util.logging.Logger.getLogger(LogExample.class.getName());
- *      @Log4j Creates log = org.apache.log4j.Logger.getLogger(LogExample.class);
- *      @Log4j2 Creates log = org.apache.logging.log4j.LogManager.getLogger(LogExample.class);
- *      @Slf4j Creates log = org.slf4j.LoggerFactory.getLogger(LogExample.class);
- *      @XSlf4j Creates log = org.slf4j.ext.XLoggerFactory.getXLogger(LogExample.class);
- *
- * 注意，需要引入对应的包
  * @author vector
+ * @Log：根据不同的注解生成不同类型的log对象，但是实例名称都是log，有六种可选实现类
+ * @CommonsLog Creates log = org.apache.commons.logging.LogFactory.getLog(LogExample.class);
+ * @Log Creates log = java.util.logging.Logger.getLogger(LogExample.class.getName());
+ * @Log4j Creates log = org.apache.log4j.Logger.getLogger(LogExample.class);
+ * @Log4j2 Creates log = org.apache.logging.log4j.LogManager.getLogger(LogExample.class);
+ * @Slf4j Creates log = org.slf4j.LoggerFactory.getLogger(LogExample.class);
+ * @XSlf4j Creates log = org.slf4j.ext.XLoggerFactory.getXLogger(LogExample.class);
+ * <p>
+ * 注意，需要引入对应的包
  * @date: 2019/2/27 0027 11:02
  */
 @Slf4j
@@ -25,6 +28,16 @@ public class LogTestMain {
         log.warn("test");
         log.error("test");
 
-
+        Date date = null;
+        try {
+            System.out.println(date.getTime());
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info("==========================================");
+            log.error("error : {} ", e.getMessage());
+            log.error("error : {} ", e);
+            log.error("error : {},{} ", 1, e);
+            log.error("error: {}", ExceptionUtils.getStackTrace(e));
+        }
     }
 }
