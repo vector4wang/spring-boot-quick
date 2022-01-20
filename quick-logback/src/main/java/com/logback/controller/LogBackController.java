176090d29f9ea2449a7165b695f13c18e805bcae
vector4wang <772704457@qq.com>
2022-01-20 22:58:49 +0800

    2022年1月20日 开始整理基础目录，标准化项目建设

--- quick-logback/src/main/java/com/logback/controller/LogBackController.java
+++ quick-logback/src/main/java/com/logback/controller/LogBackController.java
@@ -10,11 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class LogBackController {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+
     @RequestMapping(value = "/logback")
     @ResponseBody
-    public String logback(){
+    public String logback() {
         for (int i = 0; i < 100000; i++) {
-            //日志级别从低到高分为TRACE < DEBUG < INFO < WARN < ERROR < FATAL，如果设置为WARN，则低于WARN的信息都不会输出。
+            // 日志级别从低到高分为TRACE < DEBUG < INFO < WARN < ERROR <
+            // FATAL，如果设置为WARN，则低于WARN的信息都不会输出。
             logger.trace("日志输出 trace");
             logger.debug("日志输出 debug");
             logger.info("日志输出 info");
---
+++ quick-parent/README.md
@@ -0,0 +1,2 @@
+## 不使用 spring-boot-starter-parent 构建
+https://blog.csdn.net/luoww1/article/details/105530536/
