5dd55cfb940a2a3dfeadc430de6867f52b951a12
vector4wang <vector4wang@qq.com>
2022-01-25 20:52:52 +0800

    2022年1月25日20:52:43

---
+++ quick-framework/base-adapter/src/main/java/com/base/adapter/ShiroConfig.java
@@ -0,0 +1,22 @@
+package com.base.adapter;
+
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author 01390942
+ * @Description
+ * @create 2022/1/25
+ * @since 1.0.0
+ */
+public class ShiroConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator defaultAAP = new DefaultAdvisorAutoProxyCreator();
+        defaultAAP.setProxyTargetClass(true);
+        return defaultAAP;
+    }
+}
