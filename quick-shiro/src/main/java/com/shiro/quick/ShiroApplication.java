caf0a26574374cd862cd74f25ae76b25b8bc9510
wangxc <772704457@qq.com>
2022-09-03 21:27:48 +0800

    shiro 学习

---
+++ quick-shiro/src/main/java/com/shiro/quick/ShiroApplication.java
@@ -0,0 +1,15 @@
+package com.shiro.quick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class ShiroApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroApplication.class);
+
+    }
+}
