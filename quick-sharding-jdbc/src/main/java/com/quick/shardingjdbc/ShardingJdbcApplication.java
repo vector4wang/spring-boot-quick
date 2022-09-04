2f6f5a7b516727d503d856def9fc51d5027c9f43
vector4wang <vector4wang@qq.com>
2022-09-04 21:00:44 +0800

    2022年09月04日21:00:31

---
+++ quick-sharding-jdbc/src/main/java/com/quick/shardingjdbc/ShardingJdbcApplication.java
@@ -0,0 +1,17 @@
+package com.quick.shardingjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author wangxc
+ * @date: 2022/8/16 22:31
+ *
+ */
+@SpringBootApplication
+public class ShardingJdbcApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ShardingJdbcApplication.class, args);
+	}
+}
---
+++ quick-sharding-jdbc/src/main/java/com/quick/shardingjdbc/service/IAddressService.java
@@ -0,0 +1,16 @@
+package com.quick.shardingjdbc.service;
+
+import com.quick.shardingjdbc.entity.Address;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author vector
+ * @since 2022-08-16
+ */
+public interface IAddressService extends IService<Address> {
+
+}
